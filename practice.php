6f99078c5bf9db62c89f6e14386099414a7b5d73
SirDave001 <82815174+SirDave001@users.noreply.github.com>
2021-05-26 11:47:53 -0700

    added php data types lesson

--- practice.php
+++ practice.php
@@ -131,6 +131,86 @@ print "<h2>" . $txt1 . "</h2>";
 print "Study PHP with " . $txt2 . "<br>";
 print $x + $y;
 
+#php data types
+//String
+//A string is a sequence of characters, like "Hello world!".
+//A string can be any text inside quotes. You can use single or double quotes:
+$x = "Hello world!";
+$y = 'Hello world!';
+
+echo $x;
+echo "<br>";
+echo $y;
+
+//Integer
+//An integer data type is a non-decimal number between -2,147,483,648 and 2,147,483,647.
+//In the following example $x is an integer. The PHP var_dump() function returns the data type and value:
+$x = 5985;
+echo $x;
+var_dump($x);
+
+//Float (floating point numbers - also called double)
+//A float (floating point number) is a number with a decimal point or a number in exponential form.
+//In the following example $x is a float. The PHP var_dump() function returns the data type and value:
+$x = 43.6;
+echo $x;
+var_dump($x)
+
+//Boolean
+//A Boolean represents two possible states: TRUE or FALSE.
+//Booleans are often used in conditional testing. You will learn more about conditional testing in a later chapter of this tutorial.
+$x = true;
+$y = false;
+
+//Array
+//An array stores multiple values in one single variable.
+//In the following example $cars is an array. The PHP var_dump() function returns the data type and value:
+$cars = array("Volvo","BMW","Toyota");
+echo $cars;
+var_dump($cars);
+
+//Object
+//*Classes and objects are the two main aspects of object-oriented programming.
+//A class is a template for objects, and an object is an instance of a class.
+//When the individual objects are created, they inherit all the properties and behaviors from the class, but each object will have different values for the properties.
+//Let's assume we have a class named Car. A Car can have properties like model, color, etc. We can define variables like $model, $color, and so on, to hold the values of these properties.
+//When the individual objects (Volvo, BMW, Toyota, etc.) are created, they inherit all the properties and behaviors from the class, but each object will have different values for the properties.
+//If you create a __construct() function, PHP will automatically call this function when you create an object from a class.
+class Car {
+    public $gearmode;
+    public $color;
+    public $model;
+    public function __construct($gearmode, $color, $model) {
+      $this->gearmode = $gearmode;
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a "  . $this->gearmode . ", " . $this->color . " " . $this->model . "!";
+    }
+  }
+
+  $myCar = new Car("automatic", "black", "Volvo");
+  echo $myCar -> message();
+  echo "<br>";
+  $myCar = new Car("manual", "red", "Toyota");
+  echo $myCar -> message();
+
+//NULL
+//Null is a special data type which can have only one value: NULL.
+//A variable of data type NULL is a variable that has no value assigned to it.
+//Tip: If a variable is created without a value, it is automatically assigned a value of NULL.
+//Variables can also be emptied by setting the value to NULL:
+
+$x = "Hello world!";
+$x = null;
+echo $x;
+var_dump($x);
+
+//Resource
+//The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP.
+//A common example of using the resource data type is a database call.
+//We will not talk about the resource type here, since it is an advanced topic.
 
 
 
